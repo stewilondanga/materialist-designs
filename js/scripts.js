ad78842e1dd92118d02e76808e0dab66afa64a54
Stewart ilondanga <stilondanga@gmail.com>
2019-06-07 21:13:21 +0300

    add js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-var navigate = (function() {
+/*var navigate = (function() {
 	$('.dd').toggle();
 	$('.dd_btn').click(function() {
 		var dataName = $(this).attr('data-name');
@@ -6,3 +6,10 @@ var navigate = (function() {
 		$('.' + dataName).toggle();
 	});
 })();
+
+$("#submit").click(function() {
+	 $('.container').animate({
+            height: 'toggle'
+        });
+	$('.ty').fadeIn(500);
+})
